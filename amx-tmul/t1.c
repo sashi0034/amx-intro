2a08e1fc703ffd7a613418d86baf71d30cfdeaef
sashi0034 <nishida.satoshi.23e@st.kyoto-u.ac.jp>
2024-05-09 10:25:43 +0900

    サンプルコードを読みながら補足

--- amx-tmul/t1.c
+++ amx-tmul/t1.c
@@ -11,7 +11,6 @@
 #include <unistd.h>
 #include <stdbool.h>
 
-
 #define MAX 1024
 #define MAX_ROWS 16
 #define MAX_COLS 64
@@ -24,27 +23,46 @@
 // Define tile config data structure
 typedef struct __tile_config
 {
+    // 0: palette_id
     uint8_t palette_id;
+
+    // 1: startRow (8b)
     uint8_t start_row;
+
+    // 2-15: reserved (must be zero)
     uint8_t reserved_0[14];
+
+    // 16-17: tile0.colsb -- bytes_per_row
+    // 18-19: tile1.colsb
+    // 20-21: tile2.colsb
+    // ...
+    // 46-47: tile15.colsb
     uint16_t colsb[16];
+
+    // 48: tile0.rows
+    // 49: tile1.rows
+    // 50: tile2.rows
+    // ...
+    // 63: tile15.rows
     uint8_t rows[16];
 } __tilecfg;
 
 /* Initialize tile config */
 static void init_tile_config(__tilecfg *tileinfo)
 {
-    int i;
+    // パレット 0 は初期化された状態
+    // パレット 1 は 8 つのタイル レジスタに分割された 8 KB のストレージで構成され、各タイルの最大サイズは 16 行 x 64 バイト
     tileinfo->palette_id = 1;
+
     tileinfo->start_row = 0;
 
-    for (i = 0; i < 1; ++i)
+    for (int i = 0; i <= 0; ++i)
     {
         tileinfo->colsb[i] = MAX_ROWS;
         tileinfo->rows[i] = MAX_ROWS;
     }
 
-    for (i = 1; i < 4; ++i)
+    for (int i = 1; i <= 3; ++i)
     {
         tileinfo->colsb[i] = MAX_COLS;
         tileinfo->rows[i] = MAX_ROWS;
@@ -85,6 +103,7 @@ static void init_buffer32(int32_t *buf, int32_t value)
 /* Set_tiledata_use() - Invoke syscall to set ARCH_SET_STATE_USE */
 static bool set_tiledata_use()
 {
+    // Linux システムコールを呼び出して、インテル AMX 機能へのアクセスを要求
     if (syscall(SYS_arch_prctl, ARCH_REQ_XCOMP_PERM, XFEATURE_XTILEDATA))
     {
         printf("\n Fail to do XFEATURE_XTILEDATA \n\n");
@@ -100,7 +119,7 @@ static bool set_tiledata_use()
 }
 
 /* Print int8_t buffer */
-static void print_buffer(int8_t *buf, int32_t rows, int32_t colsb)
+static void print_buffer8(int8_t *buf, int32_t rows, int32_t colsb)
 {
     for (int i = 0; i < rows; i++)
     {
@@ -129,7 +148,6 @@ static void print_buffer32(int32_t *buf, int32_t rows, int32_t colsb)
 
 int main()
 {
-
     __tilecfg tile_data = {0};
     int8_t src1[MAX];
     int8_t src2[MAX];
@@ -146,10 +164,10 @@ int main()
 
     // Init src matrix buffers with data
     init_buffer(src1, 2);
-    print_buffer(src1, rows, colsb);
+    print_buffer8(src1, rows, colsb);
 
-    init_buffer(src2, 2);
-    print_buffer(src2, rows, colsb);
+    init_buffer(src2, 3);
+    print_buffer8(src2, rows, colsb);
 
     // Init dst matrix buffers with data
     init_buffer32(res, 0);
@@ -157,10 +175,10 @@ int main()
     // Load tile rows from memory
     _tile_loadd(2, src1, STRIDE);
     _tile_loadd(3, src2, STRIDE);
-    _tile_loadd(1, res, STRIDE);
+    _tile_loadd(1, res, STRIDE); // TODO: これは必要か?
 
     // Compute dot-product of bytes in tiles
-    _tile_dpbssd(1, 2, 3);
+    _tile_dpbssd(1, 2, 3); // (dst, src0, src1)
 
     // Store the tile data to memory
     _tile_stored(1, res, STRIDE);
